26ebb71688ed99116ed8305c0fe1c79d22e2e3dc
MikeJohnstone <michael.johnstone@libertymutual.com>
2017-10-10 10:51:47 -0700

    whatever

--- src/app/do-gooder/do-gooder.component.ts
+++ src/app/do-gooder/do-gooder.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import { DataService } from '../data.service'; //this is pulling info from the data service
+// import { MdDialog, MdDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-do-gooder',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoGooderComponent implements OnInit {
 
-  constructor() { }
+  errorMessage: string;
+  successMessage: string;
+  needs: any[];
+  mode = 'Observable';
 
-  ngOnInit() {
+  constructor( 
+    // private dataService: DataService
+  ) {} 
+
+  ngOnInit() 
+  { 
+    // this.getNeeds();
   }
 
+  // getNeeds() { //function to pull the needs list.
+  //   this.dataService.getRecords("needs")
+  //     .subscribe(
+  //       needs => this.needs = needs,
+  //       error =>  this.errorMessage = <any>error);
+  // }
 }
