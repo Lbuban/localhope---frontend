5fd0bb1818064ce799b6c482bdd4e2bddc5b57a8
sparrowlark <30511105+sparrowlark@users.noreply.github.com>
2017-10-10 14:33:46 -0700

    added a selector to pick user or charity

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,10 +9,24 @@ import { NgForm } from '@angular/forms';
 export class RegistrationComponent implements OnInit {
 
   registrationForm: NgForm;
+  
+  role;
+  userCheck;
+    @ViewChild('registrationForm') currentForm: NgForm;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+onChange(){
+  
+  console.log(this.role)
+  event.preventDefault()
+  if (this.role=="User") 
+    {
+     this.userCheck=true}
+  else if (this.role=="Charity")  
+     {this.userCheck=false};
+  }
 }
+
